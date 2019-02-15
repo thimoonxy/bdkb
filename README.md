2b3be41932430f3c135d26f4de0649a14125490d
Simon Xie <thimoon@sina.com>
2019-02-15 16:01:39 +0800

    spark.stream from/until offset, and terminated in 60min automatically

--- README.md
+++ README.md
@@ -4,3 +4,78 @@
 Record basic `Hadoop` component practical demos, small tricks, common api usages.
 
 ![image](https://gradle.org/images/homepage/gradle-org-hero.png)
+
+
+## Example `spark.Stream`
+
+> Capture data from 5min ago until 2min ago
+
+> Topic = simonT1
+
+> Value of record = real timestamps
+
+> Spark Streaming progress will be terminated in 60s automatically
+
+```
+Current Time: 2019-02-15 15:31:48
+
+Capture time offsets:
+partition = 0, time = 2019-02-15 15:27:01, offset = 115
+partition = 3, time = 2019-02-15 15:26:51, offset = 123
+partition = 1, time = 2019-02-15 15:26:56, offset = 111
+partition = 2, time = 2019-02-15 15:27:06, offset = 97
+Done
+
+Capture time offsets:
+partition = 0, time = 2019-02-15 15:29:51, offset = 119
+partition = 3, time = 2019-02-15 15:29:56, offset = 133
+partition = 1, time = 2019-02-15 15:30:01, offset = 121
+partition = 2, time = 2019-02-15 15:30:26, offset = 109
+Done
+
+appname=SimonStream, appId=local-1550215930545
+before terminate: 2019-02-15 15:32:11
+key=simonT1#3#123,value=2019-02-15 15:26:51
+key=simonT1#0#115,value=2019-02-15 15:27:01
+key=simonT1#3#124,value=2019-02-15 15:27:16
+key=simonT1#0#116,value=2019-02-15 15:28:21
+key=simonT1#3#125,value=2019-02-15 15:27:26
+key=simonT1#0#117,value=2019-02-15 15:28:31
+key=simonT1#3#126,value=2019-02-15 15:27:41
+key=simonT1#0#118,value=2019-02-15 15:28:51
+key=simonT1#3#127,value=2019-02-15 15:28:16
+key=simonT1#0#119,value=2019-02-15 15:29:51
+key=simonT1#3#128,value=2019-02-15 15:28:41
+key=simonT1#3#129,value=2019-02-15 15:29:01
+key=simonT1#3#130,value=2019-02-15 15:29:06
+key=simonT1#3#131,value=2019-02-15 15:29:16
+key=simonT1#3#132,value=2019-02-15 15:29:46
+key=simonT1#3#133,value=2019-02-15 15:29:56
+key=simonT1#1#111,value=2019-02-15 15:26:56
+key=simonT1#1#112,value=2019-02-15 15:27:11
+key=simonT1#1#113,value=2019-02-15 15:27:46
+key=simonT1#1#114,value=2019-02-15 15:27:51
+key=simonT1#1#115,value=2019-02-15 15:28:01
+key=simonT1#1#116,value=2019-02-15 15:28:06
+key=simonT1#1#117,value=2019-02-15 15:29:11
+key=simonT1#1#118,value=2019-02-15 15:29:21
+key=simonT1#1#119,value=2019-02-15 15:29:26
+key=simonT1#1#120,value=2019-02-15 15:29:41
+key=simonT1#1#121,value=2019-02-15 15:30:01
+key=simonT1#2#97,value=2019-02-15 15:27:06
+key=simonT1#2#98,value=2019-02-15 15:27:21
+key=simonT1#2#99,value=2019-02-15 15:27:31
+key=simonT1#2#100,value=2019-02-15 15:27:36
+key=simonT1#2#101,value=2019-02-15 15:27:56
+key=simonT1#2#102,value=2019-02-15 15:28:11
+key=simonT1#2#103,value=2019-02-15 15:28:26
+key=simonT1#2#104,value=2019-02-15 15:28:36
+key=simonT1#2#105,value=2019-02-15 15:28:46
+key=simonT1#2#106,value=2019-02-15 15:28:56
+key=simonT1#2#107,value=2019-02-15 15:29:31
+key=simonT1#2#108,value=2019-02-15 15:29:36
+key=simonT1#2#109,value=2019-02-15 15:30:26
+after terminate: 2019-02-15 15:32:41
+
+Process finished with exit code 0
+```
--- src/main/java/com/simon/spark/Stream.scala
+++ src/main/java/com/simon/spark/Stream.scala
@@ -2,7 +2,7 @@ package com.simon.spark
 
 import java.text.SimpleDateFormat
 import java.util.{Date, Properties}
-import java.util.{List => JList, Locale, Map => JMap, Set => JSet}
+import java.util.{List => JList, Locale, Map => JMap, Set => JSet, ArrayList => JArrayList, HashMap => JHashMap}
 import java.lang.{Integer => JInt, Long => JLong, Number => JNumber}
 
 import kafka.common.TopicAndPartition
@@ -17,6 +17,56 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 import scala.collection.JavaConversions._
 
 object Stream {
+
+  /**
+  *  Kafka client
+  *  - get offset from specific timestamp.
+  */
+  def getOffsetFromTime(props: Properties, topicStr:String, fetchDataTime: Long, df: SimpleDateFormat): Map[TopicAndPartition, Long] ={
+      var fromOffsets: Map[TopicAndPartition, Long] = Map()
+      val consumer = new KafkaConsumer(props)
+      try {
+          val partitionInfos = consumer.partitionsFor(topicStr)
+          val topicPartitions = new JArrayList[TopicPartition]
+          val timestampsToSearch = new JHashMap[TopicPartition, Long]
+
+          for(partitionInfo <-partitionInfos) {
+              topicPartitions.add(new TopicPartition(partitionInfo.topic(), partitionInfo.partition()))
+              timestampsToSearch.put(new TopicPartition(partitionInfo.topic(), partitionInfo.partition()), fetchDataTime)
+          }
+          //consumer.assign(topicPartitions)
+
+          val m = consumer.offsetsForTimes(timestampsToSearch.asInstanceOf[JMap[TopicPartition, JLong]])
+          var  offsetTimestamp: OffsetAndTimestamp = null
+          var  tpp: TopicPartition = null
+          println("\nCapture time offsets:")
+          for( entry <- m ) {
+              tpp = entry._1
+              offsetTimestamp = entry._2
+              if(offsetTimestamp != null) {
+                  val partition = tpp.partition()
+                  val topic = tpp.topic()
+                  val  timestamp = offsetTimestamp.timestamp()
+                  val  offset = offsetTimestamp.offset()
+                  println("partition = " + partition +
+                          ", time = " + df.format(new Date(timestamp))+
+                          ", offset = " + offset)
+                  // Needn't below stuff, we'll use spark to consume rather than consumer client api directly
+                  // consumer.seek(entry._1, offset)
+                val tpAndP = TopicAndPartition(topic, partition)
+                fromOffsets += (tpAndP-> offset)
+              }
+          }
+          println("Done\n")
+          } catch {
+          case e:Exception => e.printStackTrace()
+          } finally {
+        consumer.close()
+      }
+    fromOffsets
+  }
+
+
   def main(args: Array[String]): Unit = {
     val topics = "simonT1"
     val brokers = "localhost:9092"
@@ -36,65 +86,20 @@ object Stream {
         props.put("group.id", "simonG1")
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    val df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val now = new Date()
+    val nowTime = now.getTime()
+    println("Current Time: " + df.format(now))
+    val fetchDataTime = nowTime - 1000 * 60 * 5  // Capture offsets 5min ago, as the 'from'
+    val untilDataTime = nowTime - 1000 * 60 * 2  // Capture offsets 2min ago, as the 'until'
 
-
-    /**
-      *  Kafka client
-      *  根据时间戳获取5分钟之前的offset
-      */
-//      var fromOffsets: Map[TopicPartition, Long] = Map()
-      var fromOffsets: Map[TopicAndPartition, Long] = Map()
-      val consumer = new KafkaConsumer(props)
-      try {
-          val partitionInfos = consumer.partitionsFor(topics)
-          val topicPartitions = new java.util.ArrayList[TopicPartition]
-      //    val topicPartitions = new mutable.ListBuffer[TopicPartition]
-          val timestampsToSearch = new java.util.HashMap[TopicPartition, Long]
-          val df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-          val now = new Date()
-          val nowTime = now.getTime()
-          println("当前时间: " + df.format(now))
-          val fetchDataTime = nowTime - 1000 * 60 * 5  // 计算5分钟之前的时间戳
-          for(partitionInfo <-partitionInfos) {
-              topicPartitions.add(new TopicPartition(partitionInfo.topic(), partitionInfo.partition()))
-              timestampsToSearch.put(new TopicPartition(partitionInfo.topic(), partitionInfo.partition()), fetchDataTime)
-          }
-          consumer.assign(topicPartitions)
-          // 获取每个partition一个小时之前的偏移量
-          val m = consumer.offsetsForTimes(timestampsToSearch.asInstanceOf[JMap[TopicPartition, JLong]])
-          var  offsetTimestamp: OffsetAndTimestamp = null
-          var  tpp: TopicPartition = null
-          println("开始获取各分区初始偏移量...")
-          for( entry <- m ) {
-              // 如果设置的查询偏移量的时间点大于最大的索引记录时间，那么value就为空
-              tpp = entry._1
-              offsetTimestamp = entry._2
-              if(offsetTimestamp != null) {
-                  val partition = tpp.partition()
-                  val topic = tpp.topic()
-                  val  timestamp = offsetTimestamp.timestamp()
-                  val  offset = offsetTimestamp.offset()
-                  println("partition = " + partition +
-                          ", time = " + df.format(new Date(timestamp))+
-                          ", offset = " + offset)
-                  // 设置读取消息的偏移量
-      //            consumer.seek(entry._1, offset)
-                val tpAndP = TopicAndPartition(topic, partition)
-                fromOffsets += (tpAndP-> offset)
-              }
-          }
-          println("记录各分区初始偏移量结束...")
-          } catch {
-          case e:Exception => e.printStackTrace()
-          } finally {
-        consumer.close()
-
-      }
+    val fromOffsets = getOffsetFromTime(props, topics, fetchDataTime, df)
+    val untilOffsets = getOffsetFromTime(props, topics, untilDataTime, df)
 
     /**
       * Spark Streaming
       *
-      * 根据5分钟前的offset，读取Kafka
+      * Capture data between 5min ago and 2min ago from Kafka
       */
     val spark = SparkSession
         .builder
@@ -104,15 +109,33 @@ object Stream {
     val sc = spark.sparkContext
     sc.setLogLevel("WARN")
 
+    println(s"appname=${sc.appName}, appId=${sc.applicationId}")
+
     val ssc =  new StreamingContext(sc, Seconds(seconds.toInt))
-    val messageHandler = (mmd: MessageAndMetadata[String, String]) => (mmd.partition+"#"+mmd.offset.toString, mmd.message())
+    val messageHandler = (mmd: MessageAndMetadata[String, String]) => (mmd.topic + "#" + mmd.partition+"#"+mmd.offset.toString, mmd.message())
     val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)](ssc, kafkaParams, fromOffsets, messageHandler)
     kafkaStream.transform(rdd =>{
-      rdd.map(line => s"key=${line._1},value=${line._2}")
+      rdd.filter( (line)=> {
+        val key =  line._1
+        var res = false
+        if (key.contains("#")){
+          val Array(topic, partition, offset) = key.split("#")
+          val tpAndP = TopicAndPartition(topic, partition.trim.toInt)
+          val untilOffset = untilOffsets.getOrElse(tpAndP, 0l)
+          res = offset.trim.toLong <= untilOffset
+        }
+        res
+      }).map(line => s"key=${line._1},value=${line._2}")
     }).foreachRDD(rdd => rdd.foreach(println))
 
     ssc.start()
-    ssc.awaitTermination()
+    //ssc.awaitTermination()   // Terminate the Streaming program until manually stopped or exception occurs
+    try{
+      println(s"before terminate: ${df.format(new Date)}")
+      ssc.awaitTerminationOrTimeout(60l * 1000)   // Terminate the Streaming program in 30s
+    }finally {
+      println(s"after terminate: ${df.format(new Date)}")
+    }
 
   }
 }
